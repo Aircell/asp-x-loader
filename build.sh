df8c41b96b6d254f62693eda8953ede361c3cd5b
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-04-07 11:44:42 -0600

    See all the build output.
    Also, move the output rename above the test for success.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ workspace=$PWD/..
 PATH=/usr/lib/jvm/java-1.5.0-sun/bin:/usr/bin:/bin
 PATH+=:$workspace/cs-android-kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
 
-exec &> OUT
+#exec &> OUT
 
 # Preconfigure
 if [ ! -e include/config.mk ]; then
@@ -21,6 +21,8 @@ fi
 
 # Build
 make CROSS_COMPILE=arm-eabi- all
+scripts/signGP
+cp x-load.bin.ift MLO
 
 if [ $? -ne 0 ] || [ ! -e $target ]; then
   echo FAIL
@@ -28,9 +30,6 @@ if [ $? -ne 0 ] || [ ! -e $target ]; then
 fi
 
 ## Install
-scripts/signGP
-cp x-load.bin.ift MLO
-
 INSTALLDIR=$workspace/out
 [ -d $INSTALLDIR ] || mkdir -p $INSTALLDIR
 cp $target $INSTALLDIR
